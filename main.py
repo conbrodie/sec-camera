e494051bb12924eb0d60c7e917e79ca110085798
conbrodie <connor.h.brodie@gmail.com>
2020-10-18 00:22:46 +1000

    xml

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import threading
 
 email_update_interval = 600 # sends an email only once in this time interval
 video_camera = VideoCamera(flip=True) # creates a camera object, flip vertically
-object_classifier = cv2.CascadeClassifier("models/facial_recognition_model.xml") # an opencv classifier
+object_classifier = cv2.CascadeClassifier("models/fullbody_recognition_model.xml") # an opencv classifier
 
 # App Globals (do not edit do not edit this)
 app = Flask(__name__)
